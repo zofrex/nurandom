7b43d4044abf620147a094fc0b648569f0b4b221
James Sanderson <zofrex@gmail.com>
2017-10-07 23:19:09 +0100

    Abort if opening urandom fails or sampling rejection does not succeed after 100 attempts

--- src/nurandom.c
+++ src/nurandom.c
@@ -1,17 +1,29 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "nurandom.h"
 
+const int MAXIMUM_ITERATIONS = 100;
+
 uint32_t random_int(uint32_t upper_bound) {
   int urandom = open("/dev/urandom", O_RDONLY);
+  if(urandom < 0) {
+    abort();
+  }
   uint32_t random;
   uint32_t minimum = -upper_bound % upper_bound;
+
+  int iterations = 0;
 
   while(1) {
     read(urandom, &random, sizeof random);
     if (random > minimum) {
       break;
+    }
+    iterations++;
+    if(iterations >= MAXIMUM_ITERATIONS) {
+      abort();
     }
   }
 
